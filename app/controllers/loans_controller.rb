5c3e6dc3a4d6b0d82423e5648a2a3eec33aa91d3
Loic Thurre <loic.thurre@gmail.com>
2019-05-21 14:07:39 +0200

    deleted views from loans

--- app/controllers/loans_controller.rb
+++ app/controllers/loans_controller.rb
@@ -1,16 +1,8 @@
 class LoansController < ApplicationController
-  def new
-  end
 
   def create
     @loan = Loan.new(loan_params)
     @loan.user = current_user
     @loan.manga = Manga.find(params[:id])
-  end
-
-  private
-
-  def loan_params
-    params.require(:loan).permit(:loan_duration)
   end
 end
