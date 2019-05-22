aa6169046157b7e3ea7ee786b79d34923cd554e6
Matthieu Borgognon <matrash@bqn.ch>
2019-05-22 12:24:03 +0200

    Modified flatpickr for the lending part - side effect on all calendar => to solve

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,8 +1,18 @@
 import "bootstrap";
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/themes/airbnb.css'
+import { French } from "flatpickr/dist/l10n/fr.js"
+
+const months = ["janvier", "février", "mars", "avril", "mai", "juin", "juillet", "août", "septembre", "octobre", "novembre", "décembre"];
 
 flatpickr(".datepicker", {
-  altInput: true
+  "locale": French,
+  minDate : new Date(),
+  altInput: true,
+  altFormat: "d M Y",
+  onChange: function(dateObj) {
+    const additionalDays = parseInt(document.getElementById('loan_duration').dataset.value);
+    const d = new Date(dateObj[0].setDate(dateObj[0].getDate() + additionalDays));
+    document.getElementById('returning-date').innerHTML = d.getDate() + " " + months[d.getMonth()] + " " + d.getFullYear();
+  }
 });
-
