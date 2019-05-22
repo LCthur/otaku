592345abefa67bea3f56fd02e396562dad922e7c
Loic Thurre <loic.thurre@gmail.com>
2019-05-22 15:08:40 +0200

    added loan_state to mangas

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -6,6 +6,11 @@ class BookingsController < ApplicationController
 
   def lendings
     @user = current_user
+    # recuperation des mangas de l'utilisateur
     @mangas = Manga.where(@user.id == :user_id)
+    # pour chaque manga, definir la location
+    @mangas.each do |manga|
+      manga.loan = Loan.where(manga.id == :manga_id)
+    end
   end
 end
