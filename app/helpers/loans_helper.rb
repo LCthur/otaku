05f12e191e14d8939891e87ea78191bff4cac571
Loic Thurre <loic.thurre@gmail.com>
2019-05-22 10:58:17 +0200

    added grid cards and cards and loan_helper to know the date

---
+++ app/helpers/loans_helper.rb
@@ -0,0 +1,16 @@
+module LoansHelper
+  MONTHS = %w[Janvier Février Mars Avril Mai Juin Juillet Août Septembre Octobre Novembre Décembre]
+
+  def which_month(string)
+    month = string.split('-')[1].to_i
+    return MONTHS[month - 1]
+  end
+
+  def which_year(string)
+    string.split('-').first.to_i
+  end
+
+  def which_day(string)
+    string.split('-').last.to_i
+  end
+end
