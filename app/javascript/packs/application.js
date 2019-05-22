1d646f80982a9e4aa0e83510f8824f441a80b6e3
Matthieu Borgognon <matrash@bqn.ch>
2019-05-22 14:38:17 +0200

    Fixed ability to display two calendars for show and new manga

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,14 +5,36 @@ import { French } from "flatpickr/dist/l10n/fr.js"
 
 const months = ["janvier", "février", "mars", "avril", "mai", "juin", "juillet", "août", "septembre", "octobre", "novembre", "décembre"];
 
-flatpickr(".datepicker", {
-  "locale": French,
-  minDate : new Date(),
-  altInput: true,
-  altFormat: "d M Y",
-  onChange: function(dateObj) {
-    const additionalDays = parseInt(document.getElementById('loan_duration').dataset.value);
-    const d = new Date(dateObj[0].setDate(dateObj[0].getDate() + additionalDays));
-    document.getElementById('returning-date').innerHTML = d.getDate() + " " + months[d.getMonth()] + " " + d.getFullYear();
-  }
-});
+const daysLeft = (date, additionalDays) => {
+  return new Date(date.setDate(date.getDate() + additionalDays));
+}
+
+const flatpickrPageShow = () => {
+  flatpickr(".datepicker", {
+    "locale": French,
+    minDate : new Date(),
+    altInput: true,
+    altFormat: "d M Y",
+    onChange: function(dateObj) {
+      const additionalDays = parseInt(document.getElementById('loan_duration').dataset.value);
+      const d = daysLeft(dateObj[0], additionalDays);
+      document.getElementById('returning-date').innerHTML = d.getDate() + " " + months[d.getMonth()] + " " + d.getFullYear();
+    }
+  });
+}
+
+const flatpickrPageNewManga = () => {
+  flatpickr(".datepicker", {
+    "locale": French,
+    altInput: true,
+    altFormat: "M Y",
+  });
+}
+
+if(document.getElementById('show')){
+  flatpickrPageShow();
+};
+
+if(document.getElementById('new-manga')){
+  flatpickrPageNewManga();
+};
