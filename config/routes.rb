5f5d950c0215c693ea20e630b18e5dd0caae10d0
Loic Thurre <loic.thurre@gmail.com>
2019-05-22 09:05:28 +0200

    modified routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get '/users/:user_id/loans',       to: "bookings#loans"
-  get '/users/:user_id/lendings',    to: "bookings#lendings"
+  get '/users/:id/loans',       to: "bookings#loans"
+  get '/users/:id/lendings',    to: "bookings#lendings"
   resources :mangas, only: [:index, :show, :new, :create] do
     resources :loans, only:  [:new, :create]
   end
