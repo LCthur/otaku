252113b55d4bcd779db870dc1719370c4bbeafbc
Salcuni <giovanni.salcuni12@gmail.com>
2019-05-23 15:04:44 +0200

    add a js for display result

---
+++ app/javascript/components/search.js
@@ -0,0 +1,25 @@
+let typingTimer;                //timer identifier
+let doneTypingInterval = 3000;  //time in ms (5 seconds)
+const search = document.getElementById('search-bar');
+const spaceFormSearch = document.getElementById('form-search')
+
+//user is "finished typing," do something
+const searchKeyUp = () => {
+        search.addEventListener('keyup', (e) => {
+            console.log(e.currentTarget.value);
+            const inputUser = event.currentTarget.value;
+            clearTimeout(typingTimer);
+            if (inputUser) {
+                typingTimer = setTimeout(doneTyping, doneTypingInterval);
+            } else {
+                inputUser.remove();
+            }
+        });
+};
+
+function doneTyping () {
+    spaceFormSearch.style.marginBottom = "40px"
+    searchKeyUp();
+}
+
+export { searchKeyUp };
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,5 +1,10 @@
 import "bootstrap";
 import { flatpickrPageShow, flatpickrPageNewManga } from "../components/date";
+import { searchKeyUp } from "../components/search";
+
+if (document.getElementById('index-page')) {
+  searchKeyUp();
+};
 
 if(document.getElementById('show')){
   flatpickrPageShow();
