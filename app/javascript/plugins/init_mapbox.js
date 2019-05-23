771ce8289470cb846c0fad6e5f243e3f3b62d214
Salcuni <giovanni.salcuni12@gmail.com>
2019-05-23 17:37:47 +0200

    display a ap

---
+++ app/javascript/plugins/init_mapbox.js
@@ -0,0 +1,64 @@
+import mapboxgl from 'mapbox-gl';
+
+const initMapbox = () => {
+    const mapElement = document.getElementById('map');
+    console.log(mapElement);
+    if (mapElement) { 
+        // only build a map if there's a div#map to inject into
+      mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
+      const map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/streets-v10'
+      });
+      const markers = JSON.parse(mapElement.dataset.markers);
+      console.log(markers.first);
+      markers.forEach((marker) => {
+        new mapboxgl.Marker()
+          .setLngLat([ marker.lng, marker.lat ])
+          .addTo(map);
+      });
+    }
+  };
+  
+  export { initMapbox };
+
+// import mapboxgl from 'mapbox-gl';
+
+
+// const initMapbox = () => {
+//   const mapElement = document.getElementById('map');
+
+//   const fitMapToMarkers = (map, markers) => {
+//     const bounds = new mapboxgl.LngLatBounds();
+//     markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
+//     map.fitBounds(bounds, { padding: 70, maxZoom: 15 });
+//   };
+
+//   if (mapElement) { // only build a map if there's a div#map to inject into
+//     mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
+//     const map = new mapboxgl.Map({
+//       container: 'map',
+//       style: 'mapbox://styles/mapbox/streets-v10'
+
+//     });
+//     const markers = JSON.parse(mapElement.dataset.markers);
+//     markers.forEach((marker) => {
+
+//       const element = document.createElement('div');
+//       element.className = 'marker';
+//       element.style.backgroundSize = 'contain';
+//       element.style.width = '50px';
+//       element.style.height = '50px';
+
+//       new mapboxgl.Marker(element)
+//         .setLngLat([ marker.lng, marker.lat ])
+//         .setPopup(popup)
+//         .addTo(map);
+//     });
+//     fitMapToMarkers(map, markers);
+
+//     map.addControl(new MapboxGeocoder({ accessToken: mapboxgl.accessToken }));
+//   }
+// };
+
+// export { initMapbox };
