9a49c42c3ee6abf897f1975bc7dd6559cdbf339d
Loïc Thurre <44647346+LCthur@users.noreply.github.com>
2019-05-23 17:48:25 +0200

    Update search.js

--- app/javascript/components/search.js
+++ app/javascript/components/search.js
@@ -1,6 +1,5 @@
 const scrollUpByResult = () => {
     if (location.search) {
-        // window.location.href = "#cards-product";
         // document.querySelector('#cards-product').scrollIntoView({ 
         //     behavior: 'smooth' 
         //   });
