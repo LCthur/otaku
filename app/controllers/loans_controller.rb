3f248158582db40a9f39208d7c13618658d56160
Loic Thurre <loic.thurre@gmail.com>
2019-05-23 11:42:21 +0200

    implemented ajax to the booking form unser can't book without giving a date anymore

--- app/controllers/loans_controller.rb
+++ app/controllers/loans_controller.rb
@@ -8,9 +8,15 @@ class LoansController < ApplicationController
     @loan.user = current_user
     @loan.manga = Manga.find(params[:manga_id])
     if @loan.save
-      redirect_to users_loans_path
+      respond_to do |format|
+        format.html { redirect_to users_loans_path }
+        format.js { render js: "window.location='#{users_loans_path.to_sga}'" }
+      end
     else
-      redirect_to manga_path(@loan.manga)
+      respond_to do |format|
+        format.html { redirect_to manga_path(@loan.manga) }
+        format.js # <-- idem
+      end
     end
   end
 
---
+++ app/views/loans/create.js
@@ -0,0 +1,3 @@
+const errorsMessage = document.querySelector('.errors-message');
+
+errorsMessage.innerHTML = ` <p>Veuillez remplir une date svp</p> `
