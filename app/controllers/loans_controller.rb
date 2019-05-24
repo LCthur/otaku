87b308204a720e98f7b3f17a0cfb0b605ff51779
Loic Thurre <loic.thurre@gmail.com>
2019-05-24 12:14:18 +0200

    fixed missing redirection when not authenticated and refacto

--- app/controllers/loans_controller.rb
+++ app/controllers/loans_controller.rb
@@ -1,4 +1,6 @@
 class LoansController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:create]
+
   def new
     @loan = Loan.new
   end
@@ -8,18 +10,37 @@ class LoansController < ApplicationController
     @loan.user = current_user
     @manga = Manga.find(params[:manga_id])
     @loan.manga = @manga
+    current_user.nil? ? redirect_to_sign_in : save_loan
+  end
+
+  def redirect_to_sign_in
+    respond_to do |format|
+      format.html { redirect_to new_user_session_path }
+      format.js { render js: "window.location='#{new_user_session_path.to_s}'" }
+    end
+  end
+
+  def redirect_to_loans_recap
+    respond_to do |format|
+      format.html { redirect_to users_loans_path }
+      format.js { render js: "window.location='#{users_loans_path.to_s}'" }
+    end
+  end
+
+  def redirect_to_show
+    respond_to do |format|
+      format.html { redirect_to manga_path(@loan.manga) }
+      format.js # <-- idem
+    end
+  end
+
+  def save_loan
     if @loan.save
       @manga.loan_state = true
       @manga.save
-      respond_to do |format|
-        format.html { redirect_to users_loans_path }
-        format.js { render js: "window.location='#{users_loans_path.to_s}'" }
-      end
+      redirect_to_loans_recap
     else
-      respond_to do |format|
-        format.html { redirect_to manga_path(@loan.manga) }
-        format.js # <-- idem
-      end
+      redirect_to_show
     end
   end
 
