44468a14fea9c2fa6eca8ff26a325aa87edb70d8
Matthieu Borgognon <matrash@bqn.ch>
2019-05-24 14:55:03 +0200

    Removed manga_api.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,8 +1,5 @@
 import 'bootstrap';
 import { flatpickrPageShow, flatpickrPageNewManga } from '../components/date';
-import { retrieveMangaData } from '../components/manga_api';
-
-retrieveMangaData(1223);
 
 if(document.getElementById('show')){
   flatpickrPageShow();
